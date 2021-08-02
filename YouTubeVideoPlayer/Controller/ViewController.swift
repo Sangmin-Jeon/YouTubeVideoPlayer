b88d6b98b03178da9aecbaff151d776d2b82f6ad
sangmin-Jeon <j3k2kk22@gmail.com>
2021-08-03 02:35:43 +0900

    navigation bar title 설정

--- YouTubeVideoPlayer/Controller/ViewController.swift
+++ YouTubeVideoPlayer/Controller/ViewController.swift
@@ -50,6 +50,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        self.navigationItem.title = "My YouTube List"
+
         // navigationBtn 추가 >
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showMenu(_:)))
         
