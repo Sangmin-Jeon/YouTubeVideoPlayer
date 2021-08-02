ce8e0f67bd7cbc97b22d352c698b4acc91ea73b3
sangmin-Jeon <j3k2kk22@gmail.com>
2021-08-02 19:35:09 +0900

    model 생성

---
+++ YouTubeVideoPlayer/Model/model.swift
@@ -0,0 +1,16 @@
+//
+//  model.swift
+//  YouTubeVideoPlayer
+//
+//  Created by 전상민 on 2021/08/02.
+//
+
+import UIKit
+
+struct model {
+    
+    var name: String = ""
+    var list: [String] = ["https://youtu.be/tnGaCZZ5Z28"]
+
+}
+
