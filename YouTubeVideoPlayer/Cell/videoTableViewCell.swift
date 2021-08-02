d0d8961003582450722e0304a0b82597ce99681e
sangmin-Jeon <j3k2kk22@gmail.com>
2021-08-02 19:36:14 +0900

    tableVIewCell 생성 및 VC연결

---
+++ YouTubeVideoPlayer/Cell/videoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  videoTableViewCell.swift
+//  YouTubeVideoPlayer
+//
+//  Created by 전상민 on 2021/08/02.
+//
+
+import UIKit
+
+class videoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ YouTubeVideoPlayer/Controller/videoViewController.swift
@@ -0,0 +1,27 @@
+//
+//  videoViewController.swift
+//  YouTubeVideoPlayer
+//
+//  Created by 전상민 on 2021/08/02.
+//
+
+import UIKit
+import YoutubePlayer_in_WKWebView
+
+class videoViewController: UIViewController {
+    
+    // view의 비율: 16:9로 설정 
+    @IBOutlet weak var videoView: WKYTPlayerView!
+    
+    // playsinline
+    // 1: 기본 view에서 재생, 0: 전체화면에서 재생
+    let playVarsDic = ["playsinline": 0]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        videoView.load(withVideoId: "tnGaCZZ5Z28", playerVars: playVarsDic)
+    }
+
+
+}
